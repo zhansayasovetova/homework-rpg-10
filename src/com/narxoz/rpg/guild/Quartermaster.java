4ec432cb5a67f9fb37f6eafa286922261571913e
zhansayasovetova <zhansaya.sovetova@narxoz.kz>
2026-05-11 22:57:22 +0500

    implement quartermaster supply coordination

--- src/com/narxoz/rpg/guild/Quartermaster.java
+++ src/com/narxoz/rpg/guild/Quartermaster.java
@@ -10,12 +10,31 @@ public class Quartermaster extends GuildMember {
     }
 
     public void requestSupplies(String topic, String payload) {
-        // TODO: send a supply-related message through the mediator.
+
         getMediator().dispatch(topic, this, payload);
     }
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+
+        System.out.println(
+                "[Quartermaster " + getName() + "] received topic '" +
+                        topic + "' from " + from.getName() +
+                        ": " + payload
+        );
+
+        if ("quest".equalsIgnoreCase(topic)) {
+
+            System.out.println(
+                    "[Quartermaster " + getName() + "] prepares weapons, food, and rewards."
+            );
+        }
+
+        if ("supplies".equalsIgnoreCase(topic)) {
+
+            System.out.println(
+                    "[Quartermaster " + getName() + "] checks inventory and resource stock."
+            );
+        }
     }
 }
