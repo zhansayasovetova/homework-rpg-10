f466f67cf8ac9c8e6e95d99f57750ecbfe09b20e
zhansayasovetova <zhansaya.sovetova@narxoz.kz>
2026-05-11 22:58:07 +0500

    implement guild war council planning workflow

--- src/com/narxoz/rpg/council/CouncilEngine.java
+++ src/com/narxoz/rpg/council/CouncilEngine.java
@@ -1,8 +1,13 @@
 package com.narxoz.rpg.council;
 
 import com.narxoz.rpg.combatant.Hero;
+import com.narxoz.rpg.guild.Captain;
 import com.narxoz.rpg.guild.GuildMediator;
+import com.narxoz.rpg.quest.Quest;
+import com.narxoz.rpg.quest.QuestIterator;
 import com.narxoz.rpg.quest.QuestLog;
+import com.narxoz.rpg.quest.QuestPriority;
+
 import java.util.List;
 
 /**
@@ -11,9 +16,58 @@ import java.util.List;
 public class CouncilEngine {
 
     public CouncilRunResult runCouncil(List<Hero> party, QuestLog questLog, GuildMediator hall) {
-        // TODO: walk questLog with at least 2 different iterators,
-        //       dispatch coordinating messages through hall for each quest,
-        //       and return counters (questsTraversed, messagesRouted, membersNotified).
-        return new CouncilRunResult(0, 0, 0);
+
+        int questsTraversed = 0;
+        int messagesRouted = 0;
+        int membersNotified = 0;
+
+        Captain councilLeader = new Captain("Council Leader", hall);
+
+        System.out.println("\n=== WAR COUNCIL STARTED ===");
+
+        System.out.println("\nParty members:");
+        for (Hero hero : party) {
+            System.out.println(hero);
+        }
+
+        System.out.println("\n--- Ordered quest traversal ---");
+        QuestIterator orderedIterator = questLog.ordered();
+
+        while (orderedIterator.hasNext()) {
+            Quest quest = orderedIterator.next();
+            questsTraversed++;
+
+            System.out.println("Planning quest: " + quest);
+
+            hall.dispatch("quest", councilLeader, "Plan mission: " + quest);
+            messagesRouted++;
+            membersNotified += 2;
+        }
+
+        System.out.println("\n--- High priority quest traversal ---");
+        QuestIterator priorityIterator = questLog.priorityAtLeast(QuestPriority.HIGH);
+
+        while (priorityIterator.hasNext()) {
+            Quest quest = priorityIterator.next();
+            questsTraversed++;
+
+            System.out.println("Urgent review: " + quest);
+
+            hall.dispatch("danger", councilLeader, "High priority danger detected: " + quest);
+            messagesRouted++;
+            membersNotified += 2;
+
+            hall.dispatch("healing", councilLeader, "Prepare healing support for: " + quest);
+            messagesRouted++;
+            membersNotified += 2;
+
+            hall.dispatch("supplies", councilLeader, "Prepare supplies for: " + quest);
+            messagesRouted++;
+            membersNotified += 2;
+        }
+
+        System.out.println("\n=== WAR COUNCIL FINISHED ===");
+
+        return new CouncilRunResult(questsTraversed, messagesRouted, membersNotified);
     }
 }
